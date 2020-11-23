0c86ce095ac1515d962c19aabce23fd124694f38
John Zhang <1724118498@qq.com>
2020-11-24 02:36:38 +0800

    push

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-function init() {
+import * as yelp from "./yelp.js";
+import * as ajax from "./ajax.js";
 
+let business;
+
+function init() {
+    let url = yelp.initYelp();
+
+    function businessLoaded(jsonString) {
+        business = JSON.parse(jsonString);
+        console.log(business);
+    }
+    
+    ajax.downloadFile(url,)
 }
 
 export {
